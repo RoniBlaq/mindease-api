6529ca72c38d82c58d6432fdf6a64a93ed6c70a4
RoniBlaq <adeotiaderonke565@gmail.com>
2026-08-05 13:53:12 +0100

    Configure Railway MySQL connection

--- db.php
+++ db.php
@@ -1,13 +1,38 @@
 <?php
-$host ="";
-$user = "";
-$password = "";
-$database = "";
+require __DIR__ . '/vendor/autoload.php';
 
-$conn = new mysqli($host, $user, $password, $database);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
-if ($conn->connect_error) {
-    die("connection failed: " . $conn->connect_error);
-}
+$dbHost = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$dbUser = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$dbPass = $_ENV['MYSQLPASSWORD']?? getenv('MYSQLPASSWORD');
+$dbName = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$dbPort = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
+
+$conn = new mysqli( 
+    $dbHost,
+    $dbUser,
+    $dbPass,
+    $dbName,
+    (int)$dbPort
+    );
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->set_charset("utf8mb4");
+
+// $host ="";
+// $user = "";
+// $password = "";
+// $database = "";
+
+// $conn = new mysqli($host, $user, $password, $database);
+
+// if ($conn->connect_error) {
+//     die("connection failed: " . $conn->connect_error);
+// }
+
 
 ?>
+?>/
