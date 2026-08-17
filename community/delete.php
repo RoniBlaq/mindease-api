84d2188aa5614a328c12e48ea5357c3ed8a9a48f
RoniBlaq <adeotiaderonke565@gmail.com>
2026-08-17 13:31:42 +0100

    Likes Button Fixed

--- community/delete.php
+++ community/delete.php
@@ -59,5 +59,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
                                         "status" => "error" 
                                              ]);
                                               }
-                                            $conn->close(); 
+                                             $conn->close(); 
                                                ?>
