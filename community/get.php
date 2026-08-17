dd935ca4b6dd2f57ba63d141eb41549f8376f609
RoniBlaq <adeotiaderonke565@gmail.com>
2026-08-17 13:49:59 +0100

    Fixed bug

--- community/get.php
+++ community/get.php
@@ -1,11 +1,9 @@
 <?php 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
-header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Origin: https://mindease-smoky.vercel.app");
 header("Access-Control-Allow-Headers: Content-Type");
- header("Access-Control-Allow-Methods: POST, OPTIONS");
+ header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
